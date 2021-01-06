ad7205819b7f1738f7f4c186a02c30d1c56b8158
rdoby99 <ramonadoby@gmail.com>
2021-01-06 01:43:58 -0500

    Finished project

--- loancalculator.js
+++ loancalculator.js
@@ -1,1 +1,20 @@
+let loanAmount = document.querySelector('#amount');
+let interestRate = document.querySelector('#interest_rate');
+let months = document.querySelector('#months');
+let loanCal = document.querySelector('#loancal');
+let payment = document.querySelector('#payment');
+
+interestRate.setAttribute('type', 'number');
+
+function computeLoan() {
+    let loanAmountValue = Number(loanAmount.value);
+    let interestRateValue = Number(interestRate.value/100);
+    let monthsValue = Number(months.value);
+    function numberWithCommas() {
+        let finalAmount = ((loanAmountValue + (loanAmountValue * interestRateValue)) / monthsValue).toFixed(2);
+        return finalAmount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    };
+    payment.textContent = 'Monthy Payment = $' + numberWithCommas();
+};
+
 
