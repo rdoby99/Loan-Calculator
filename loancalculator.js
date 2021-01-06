7d1c0b19fd39a9fb7190d6f445a56abce234f5a6
rdoby99 <ramonadoby@gmail.com>
2021-01-06 03:13:34 -0500

    Updated JS Code After Reflection

--- loancalculator.js
+++ loancalculator.js
@@ -1,3 +1,6 @@
+// My Original, working code
+
+/*
 let loanAmount = document.querySelector('#amount');
 let interestRate = document.querySelector('#interest_rate');
 let months = document.querySelector('#months');
@@ -15,5 +18,20 @@ function computeLoan() {
     };
     payment.textContent = 'Monthy Payment = $' + numberWithCommas();
 };
+*/
+
+// Code after improvements and reflection (See README.md)
+interestRate.setAttribute('type', 'number');
+
+function computeLoan() {
+    let loanAmount = Number(document.querySelector('#amount').value);
+    let interestRate = Number(document.querySelector('#interest_rate').value/100);
+    let months = Number(document.querySelector('#months').value);
+    let payment = document.querySelector('#payment');
+    let finalAmount = ((loanAmount + (loanAmount * interestRate)) / months).toFixed(2);
+    finalAmount = finalAmount.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    payment.textContent = 'Monthy Payment = $' + finalAmount;
+};
+
 
 
